409c5d4c3117f50588c376a2b717014dec9378f7
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2011-05-01 21:40:09 +0200

    initial localisation support and resource file

---
+++ locales/df_es.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="MainWindow.py" line="75"/>
+        <source>Unexpected error: %s; %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DecoyFinderThread</name>
+    <message>
+        <location filename="MainWindow.py" line="53"/>
+        <source>Reading files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.py" line="70"/>
+        <source>Reading %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="Ui_MainWindow.py" line="171"/>
+        <source>Decoy Finder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="184"/>
+        <source>Query</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="172"/>
+        <source>Query Molecules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="173"/>
+        <source>Add query files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="174"/>
+        <source>Database files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="175"/>
+        <source>Select local files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="176"/>
+        <source>ZINC single</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="177"/>
+        <source>ZINC metals</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="178"/>
+        <source>ZINC usual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="179"/>
+        <source>ZINC all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="180"/>
+        <source>Add DB files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="181"/>
+        <source>Output directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="182"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="183"/>
+        <source>Find decoys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="193"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="185"/>
+        <source>Tanimoto &lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Ui_MainWindow.py" line="186"/>
+        <source>HBA ±</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Ui_MainWindow.py" line="187"/>
+        <source>HBD ±</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Ui_MainWindow.py" line="188"/>
+        <source>ClogP ±</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Ui_MainWindow.py" line="189"/>
+        <source>Molecular weight ±</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Ui_MainWindow.py" line="190"/>
+        <source>Rotational bonds ±</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="191"/>
+        <source> Da</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="192"/>
+        <source>Load Defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="194"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Ui_MainWindow.py" line="195"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.py" line="100"/>
+        <source>Molecule files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.py" line="118"/>
+        <source>Done.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.py" line="193"/>
+        <source>Unable to read selected directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.py" line="213"/>
+        <source>You must select at least one file containing quey molecules, and at least one molecule library file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
