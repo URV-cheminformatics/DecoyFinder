24037b5142659196468c2a7170af816c5f0d69b1
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2011-07-24 15:02:31 +0200

    now it's easier to change the project url

--- decoy_finder.py
+++ decoy_finder.py
@@ -28,7 +28,7 @@ from PySide.QtCore import QTranslator, QLocale
 ORGNAME = u'Universitat Rovira i Virgili - Grup de Recerca en Nutrigenòmica'
 NAME = 'DecoyFinder'
 VERSION = '1.0'
-URL = 'http://www.ctns.cat/DecoyFinder'
+URL = 'www.ctns.cat/DecoyFinder'
 
 def main():
     """
