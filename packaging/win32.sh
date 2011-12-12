8406163a9973821ca168d5f69338c9399c39172b
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2011-12-12 15:23:50 +0100

    adapt win32 scripts to be more generic

--- packaging/win32.sh
+++ packaging/win32.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ./common.sh
 export WINEPREFIX=$HOME/.wine
-export PI=/home/ssorgatem/winbin/pyinstaller-1.5.1/pyinstaller.py
+export PI=$HOME/winbin/pyinstaller-1.5.1/pyinstaller.py
 rm -r DecoyFinder*.exe warnDecoyFinder.txt build
 wine c:/Python27/python $PI -w -i ../icon.ico -X -n DecoyFinder -F  DecoyFinder.spec
 makensis installer.nsi
