5b6f448f0b47e11b96b1f8dd3f437f9f4c5f2dfc
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2011-12-12 15:21:30 +0100

    fix icon location

--- packaging/win32.sh
+++ packaging/win32.sh
@@ -3,5 +3,5 @@
 export WINEPREFIX=$HOME/.wine
 export PI=/home/ssorgatem/winbin/pyinstaller-1.5.1/pyinstaller.py
 rm -r DecoyFinder*.exe warnDecoyFinder.txt build
-wine c:/Python27/python $PI -w -i icon.ico -X -n DecoyFinder -F  DecoyFinder.spec
+wine c:/Python27/python $PI -w -i ../icon.ico -X -n DecoyFinder -F  DecoyFinder.spec
 makensis installer.nsi
