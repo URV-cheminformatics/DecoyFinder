57092498823dac3719000660866ca2507a57a96d
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2011-12-20 17:01:08 +0100

    forgot to export the PYTHONPATH

--- packaging/linux32.sh
+++ packaging/linux32.sh
@@ -3,5 +3,5 @@
 OBDIR=/opt/openbabel-2.3.1
 export BABEL_DATADIR=$OBDIR/share/openbabel/2.3.1
 export BABEL_LIBDIR=$OBDIR/lib/openbabel/2.3.1
-PYTHONPATH=$OBDIR/lib/python2.6/site-packages:$PYTHONPATH
+export PYTHONPATH=$OBDIR/lib/python2.6/site-packages:$PYTHONPATH
 python $PI -C $PIDIR/config-linux32.dat -F  DecoyFinder.spec
