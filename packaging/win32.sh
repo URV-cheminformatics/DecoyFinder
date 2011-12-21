b2ee45c963e07e1da91a05d36b724f5b66032cee
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2011-12-21 21:56:25 +0100

    use local config files for PI

--- packaging/win32.sh
+++ packaging/win32.sh
@@ -2,5 +2,5 @@
 . ./common.sh
 export WINEPREFIX=$HOME/.wine
 rm -r DecoyFinder*.exe warnDecoyFinder.txt build
-wine c:/Python27/python $PI -w -X -n DecoyFinder -F  DecoyFinder.spec
+wine c:/Python27/python $PI -w -X -C config-win32.dat  -F  DecoyFinder.spec
 makensis installer.nsi
