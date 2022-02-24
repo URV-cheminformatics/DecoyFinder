1eba674cd93ef6320c8584a4377c6fc7b74e4710
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2022-02-24 09:03:55 +0100

    fix file mode

--- find_decoys.py
+++ find_decoys.py
@@ -709,7 +709,7 @@ def find_decoys(
         log += '"%s","%s","%s","%s","%s","%s","%s"\n' % tuple([str(f) for f in (active.title,  active.hba,  active.hbd,  active.clogp,  active.mw,  active.rot,  ligands_dict[active])])
     log += "\n"
 
-    logfile = open('%s_log.csv' % outputfile,  'wb')
+    logfile = open('%s_log.csv' % outputfile,  'wt')
     logfile.write(log)
     logfile.close()
 
